03d60985765c04f46a8983ad1456916e3286402b
apple <864849506@qq.com>
2017-02-06 16:23:03 +0800

    加载授权页面

--- Weibo/WBCommon.swift
+++ Weibo/WBCommon.swift
@@ -8,6 +8,14 @@
 
 import Foundation
 
+//MARK : - 应用程序信息
+// 应用程序 ID
+let WBAppKey = "2247319834"
+// 应用程序加密信息(开发者可修改)
+let WBAppSecret = "17ef00e9d21ea58a2c322accecad55fe"
+// 回调地址
+let WBRedirectURL = "http://baidu.com"
+
 //MARK : - 微博全局通知定义
 //用户需要通知类型
 let WBUserShouldLoginNotification = "WBUserShouldLoginNotification"
--- Weibo/WBOAuthViewController.swift
+++ Weibo/WBOAuthViewController.swift
@@ -27,7 +27,14 @@ class WBOAuthViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // 加载授权页面
+        let urlString = "https://api.weibo.com/oauth2/authorize?client_id=\(WBAppKey)&redirect_uri=\(WBRedirectURL)"
+        // 1> 确定要访问的资源
+        let url = URL(string: urlString)
+        // 2> 建立请求
+        let request = URLRequest(url: url!)
+        // 3> 加载请求
+        webView.loadRequest(request)
     }
 
     func close(){

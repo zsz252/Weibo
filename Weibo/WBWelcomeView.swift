dcfab9a605da17b4c23c802026be64e59ab57ea1
apple <864849506@qq.com>
2017-02-17 22:18:20 +0800

    欢迎界面自动布局动画实现

--- Weibo/WBWelcomeView.swift
+++ Weibo/WBWelcomeView.swift
@@ -12,6 +12,12 @@ import UIKit
 /// 欢迎视图
 class WBWelcomeView: UIView {
 
+    @IBOutlet weak var iconView: UIImageView!
+    
+    @IBOutlet weak var tipLable: UILabel!
+    
+    @IBOutlet weak var bottomCons: NSLayoutConstraint!
+    
     class func welcomeView() -> WBWelcomeView{
         
         let nib = UINib(nibName: "WBWelcomeView", bundle: nil)
@@ -21,5 +27,26 @@ class WBWelcomeView: UIView {
 //        v.frame = UIScreen.main().bounds
         
         return v as! WBWelcomeView
+    }
+    
+    /// 视图被添加到 window上
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
+        
+        self.layoutIfNeeded()
+        
+        bottomCons.constant = bounds.size.height - 200
+        
+        UIView.animate(withDuration: 1.0,
+                       delay: 0,
+                       usingSpringWithDamping: 0.7,
+                       initialSpringVelocity: 0,
+                       options: [],
+                       animations: { 
+                        //更新约束
+                        self.layoutIfNeeded()
+            }) { (_) in
+                
+        }
     }
 }

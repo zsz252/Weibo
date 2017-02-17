169097c2308677f4e20990f827d04c646e872626
apple <864849506@qq.com>
2017-02-17 22:54:31 +0800

    在 awakeFromNib 加载图像

--- Weibo/WBWelcomeView.swift
+++ Weibo/WBWelcomeView.swift
@@ -8,7 +8,7 @@
 //
 
 import UIKit
-
+import SDWebImage
 /// 欢迎视图
 class WBWelcomeView: UIView {
 
@@ -29,15 +29,29 @@ class WBWelcomeView: UIView {
         return v as! WBWelcomeView
     }
     
+    // initWithCoder
+    override func awakeFromNib() {
+        // 1. url
+        guard let urlString = WBNetworkManager.shared.userAccount.avatar_large else{
+            return
+        }
+        let url = URL(string: urlString)
+        // 2. 设置头像
+        iconView.sd_setImage(with: url, placeholderImage: UIImage(named: "profile"))
+        
+        iconView.layer.cornerRadius = 42.5
+        iconView.layer.masksToBounds = true
+    }
+    
     /// 视图被添加到 window上
     override func didMoveToWindow() {
         super.didMoveToWindow()
         
         self.layoutIfNeeded()
         
-        bottomCons.constant = bounds.size.height - 200
+        bottomCons.constant = bounds.size.height - 260
         
-        UIView.animate(withDuration: 1.0,
+        UIView.animate(withDuration: 2.0,
                        delay: 0,
                        usingSpringWithDamping: 0.7,
                        initialSpringVelocity: 0,
@@ -46,7 +60,9 @@ class WBWelcomeView: UIView {
                         //更新约束
                         self.layoutIfNeeded()
             }) { (_) in
-                
+                UIView.animate(withDuration: 1.0, animations: { 
+                    self.tipLable.alpha = 1
+                })
         }
     }
 }

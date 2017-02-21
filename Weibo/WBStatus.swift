59fd1e30d34f5c6ab8fb7e9b663ab61b5572df75
apple <864849506@qq.com>
2017-02-21 10:27:22 +0800

    实现微博用户的字典转模型

--- Weibo/WBStatus.swift
+++ Weibo/WBStatus.swift
@@ -17,6 +17,9 @@ class WBStatus: NSObject {
     //微博信息内容
     var text:String?
     
+    //微博的用户
+    var user:WBUser?
+    
     //重写 description 的计算性属性
     override var description: String{
         return yy_modelDescription()

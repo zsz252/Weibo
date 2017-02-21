438b5c09cedcfc1432797ef54bf8356134e271dc
apple <864849506@qq.com>
2017-02-21 10:51:34 +0800

    字典转模型

--- Weibo/WBStatusListViewModel.swift
+++ Weibo/WBStatusListViewModel.swift
@@ -11,16 +11,37 @@ import Foundation
 class WBStatusListViewModel {
     
     //微博数据模型懒加载
-    lazy var statusList = [WBStatus]()
+    lazy var statusList = [WBStatusViewModel]()
     
+    /// 加载微博列表
+    ///
+    /// - parameter pullup:     是否上拉刷新标记
+    /// - parameter completion: 完成回调(网络请求是否成功，是否刷新表格)
     func loadStatus(pullup:Bool,completion: @escaping (_ isSucess:Bool)->()){
         
-        
-        let since_id = pullup ? 0 : (self.statusList.first?.id ?? 0)
-        
-        let max_id = pullup ? (self.statusList.last?.id) ?? 0 : 0
+        // since_id 取出数组中第一条微博的id
+        let since_id = pullup ? 0 : (self.statusList.first?.status.id ?? 0)
+        // 上拉刷新，取出数组最后一条微博的id
+        let max_id = pullup ? (self.statusList.last?.status.id) ?? 0 : 0
         
         WBNetworkManager.shared.statusList(since_id:since_id,max_id: max_id)  { (list, isSucess) in
+            //判断网络请求是否成功
+            if !isSucess{
+                return
+            }
+            
+            // 定义结果可变数组
+            var array = [WBStatusViewModel]()
+            
+            // 遍历服务器返回的字典数组，字典转模型
+            for dict in list {
+                
+                // 创建微博模型
+                let model = WBStatus.yy_model(with: dict)
+                
+                // 将 视图模型 添加到数组
+                array.append(WBStatusViewModel(model: model!))
+            }
             
             //字典转模型
             guard let array = NSArray.yy_modelArray(with: WBStatus.classForCoder(), json: list) as? [WBStatus] else {

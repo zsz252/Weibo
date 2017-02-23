97d9dd1bbd21c8c0ec527de5e6ebe2171b5dfdf1
apple <864849506@qq.com>
2017-02-24 00:31:18 +0800

    循环设置图像

--- Weibo/WBStatusPictureView.swift
+++ Weibo/WBStatusPictureView.swift
@@ -9,7 +9,34 @@
 import UIKit
 
 class WBStatusPictureView: UIView {
-
+    
+    // 配图视图数组
+    var urls:[WBStatusPicture]?{
+        didSet{
+            
+            // 隐藏所有的imageview
+            for v in subviews {
+                v.isHidden = true
+            }
+    
+            // 遍历 urls 数组，顺序设置图像
+            var index = 0
+            for url in urls ?? []{
+                
+                // 获得对应索引的 imageView
+                let iv = subviews[index] as! UIImageView
+                
+                // 设置图像
+                iv.wb_setImage(urlString: url.thumbnail_pic!, placeholderImage: nil)
+                
+                // 显示图像
+                iv.isHidden = false
+                
+                index += 1
+            }
+        }
+    }
+    
     @IBOutlet weak var heightCons: NSLayoutConstraint!
     
     override func awakeFromNib() {
@@ -29,6 +56,10 @@ extension WBStatusPictureView{
         // 循环创建 9 个 imageView
         for i in 0..<9{
             let iv = UIImageView(frame: rect)
+            
+            // 设置 contenMode
+            iv.contentMode = .scaleAspectFill
+            iv.clipsToBounds = true
             
             iv.backgroundColor = UIColor.blue
             

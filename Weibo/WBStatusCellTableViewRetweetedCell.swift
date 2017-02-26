73a8e0a9052fdf3a58f0f2f108d2b47dff5fb7f6
apple <864849506@qq.com>
2017-02-26 23:51:11 +0800

    复制被转发微博 Cell 的 XIB

---
+++ Weibo/WBStatusCellTableViewRetweetedCell.swift
@@ -0,0 +1,13 @@
+//
+//  WBStatusCellTableViewRetweetedCell.swift
+//  Weibo
+//
+//  Created by apple on 2017/2/26.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+import UIKit
+
+class WBStatusCellTableViewRetweetedCell: NSObject {
+
+}
